2ffa528c81fd00aa31f5f751a851b062f25918be
@A-McMullin <a.p.mcmullin@gmail.com>
2022-05-15 19:52:04 -0500

    HW1 Commit

--- src/GitClass01/GitHW1.java
+++ src/GitClass01/GitHW1.java
@@ -5,7 +5,7 @@ public class GitHW1 {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i<=100; i++){
+        for (int i = 0; i<=50; i++){
             System.out.println(i);
         }
     }
